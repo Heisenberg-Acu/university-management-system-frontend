db4eef658f4f3c837cb43f8f8ce48e2dbd67de43
OmarMansour22 <omarmansour996@gmail.com>
2023-12-26 18:31:05 +0200

    feat(Footer):Change Logo style in Footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,15 +4,17 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
-import '../../styles/components/common/Footer.css'
+import '../../styles/components/common/Footer.css';
+import Logo from '../../assets/common/logo.png'
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
         <div className="row">
-          <div>
-            <img
-              src="img/ImageHandler.png"
+          <div className="footer-col">
+            <img className="footer-logo"
+              src={Logo}
               alt="ACU Logo"
               width="450px"
               style={{ marginLeft: '-170px', marginTop: '-80px' }}
@@ -20,7 +22,7 @@ const Footer = () => {
           </div>
           <div className="footer-col">
             <h4>ABOUT US</h4>
-            <ul className='p-0'>
+            <ul className="p-0">
               <li><a href="#">About ACU</a></li>
               <li><a href="#">History of ACU</a></li>
               <li><a href="#">About Faculty Of Engineering</a></li>
@@ -30,7 +32,7 @@ const Footer = () => {
           </div>
           <div className="footer-col">
             <h4>Engineering Departments</h4>
-            <ul className='p-0'>
+            <ul className="p-0">
               <li><a href="#">electrical engineering</a></li>
               <li><a href="#">mechanical engineering</a></li>
               <li><a href="#">civil engineering</a></li>
@@ -41,10 +43,10 @@ const Footer = () => {
           <div className="footer-col">
             <h4>follow us</h4>
             <div className="social-links">
-              <a href="#"><FontAwesomeIcon icon={faFacebookF}/></a>
-              <a href="#"><FontAwesomeIcon icon={faTwitter}/></a>
-              <a href="#"><FontAwesomeIcon icon={faInstagram}/></a>
-              <a href="#"><FontAwesomeIcon icon={faLinkedinIn}/></a>
+              <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+              <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+              <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+              <a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a>
             </div>
           </div>
         </div>
