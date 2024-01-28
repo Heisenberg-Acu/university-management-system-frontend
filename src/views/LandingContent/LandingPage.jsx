09b5f46b1f4c16a3f50cbd792fd2d040e17b3ac1
Ali Elmala <ali.elmala22@gmail.com>
2024-01-28 16:23:10 +0200

    feat(LandingPage): finish about us section

--- src/views/LandingContent/LandingPage.jsx
+++ src/views/LandingContent/LandingPage.jsx
@@ -13,21 +13,22 @@ const LandingPage = () => {
   return (
     <div>
       <div class="hero-img">
-      <div className="dark">
-        <div class="hero-text">
-          <h1>Welcom To ACU</h1>
-          <p>Faculty Of Engineering</p>
-          <a href="#" class="hero-btn">About Us</a>
-        </div>
+        <div className="dark">
+          <div class="hero-text">
+            <h1>Welcom To ACU</h1>
+            <p>Faculty Of Engineering</p>
+            <a href="#" class="hero-btn">About Us</a>
+          </div>
         </div>
       </div>
 
       <div className='container py-5'>
-        <div className='row'>
+        <h2 className='section-title'>About Us</h2>
+        <div className='row flex-md-row flex-column-reverse justify-content-between'>
           <div className='col-md-6 mb-4 d-flex flex-column justify-content-center'>
             <p className='badge'>About Us</p>
-            <h1 className='mt-0'>Welcome to Facualty of Engineering - ACU</h1>
-            <p className='standard-paragraph'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam autem nemo unde eum minus enim doloremque, perspiciatis ex, culpa facilis tempore nulla error odit repellat a perferendis deleniti commodi fuga.</p>
+            <h1 style={{ fontWeight: 600 }} className='mt-0'>Welcome to Facualty of Engineering - ACU</h1>
+            <p className='standard-paragraph my-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam autem nemo unde eum minus enim doloremque, perspiciatis ex, culpa facilis tempore nulla error odit repellat a perferendis deleniti commodi fuga.</p>
             <ConfigProvider
               theme={{
                 token: {
@@ -46,22 +47,30 @@ const LandingPage = () => {
               <Button className='btn nav-btn mt-2'>Read more</Button>
             </ConfigProvider>
           </div>
-          <div className='col-md-6 mb-4'>
+          <div className='col-md-5 mb-4'>
+            <img src="https://placehold.co/400" alt="" style={{ width: '100%' }} />
+          </div>
+        </div>
+        <div className='row g-4 justify-content-between'>
+          <div className='col-md-5'>
             <div className='info-box'>
               <div className='info-box-icon'>
                 <FontAwesomeIcon icon={faCircleQuestion} />
               </div>
-              <h2>Mission</h2>
+              <h2 className='mt-4 mb-3'>Mission</h2>
               <p className='standard-paragraph'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsam totam quia quisquam sapiente nostrum vitae! Eos illum eius molestiae, dignissimos dolore optio deserunt ut alias nulla dolor, quaerat facilis.
               </p>
             </div>
+          </div>
+          <div className='col-md-5'>
             <div className='info-box'>
               <div className='info-box-icon'>
                 <FontAwesomeIcon icon={faLightbulb} />
               </div>
-              <h2 className='my-3'>Vision</h2>
-              <p className='standard-paragraph mb-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo repellat tenetur at sit qui illo quidem magni sunt ullam non sint eum debitis cupiditate ipsa, quod quibusdam expedita temporibus earum.</p>
+              <h2 className='mt-4 mb-3'>Vision</h2>
+              <p className='standard-paragraph'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo repellat tenetur at sit qui illo quidem magni sunt ullam non sint eum debitis cupiditate ipsa, quod quibusdam expedita temporibus earum.</p>
+
             </div>
           </div>
         </div>
